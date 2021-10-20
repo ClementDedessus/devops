e254c95f0873ba8a482bb1efde7fa5f8ec236f23
De dessus Clement <clement.dedessuslesmous@student.vinci.ipl.be>
2021-10-20 17:31:42 +0200

    add - home.php, new title

--- GoodNewsSite/views/home.php
+++ GoodNewsSite/views/home.php
@@ -1,5 +1,6 @@
 <div id="contenu">
 	<h2>Accueil</h2>
 	<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
-	<p>Jean-Luc Collinet.</p>
+
+    <p>Clement Dedessuslesmoustier.</p>
 </div><!-- #contenu -->
