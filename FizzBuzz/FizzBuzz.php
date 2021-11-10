2c24b71c181b18c524738dcc29c4c2be3bad477d
De dessus Clement <clement.dedessuslesmous@student.vinci.ipl.be>
2021-11-10 17:43:19 +0100

    add - PHPUnit

---
+++ FizzBuzz/FizzBuzz.php
@@ -0,0 +1,9 @@
+<?php
+
+class FizzBuzz
+{
+    //tous les multiples de 3 sont remplacés par fizz
+    //tous les multiples de 5 sont remplacés par buzz
+    //tous les multiples de 2 et 5 sont remplacés pas fizzbuzz
+
+}
