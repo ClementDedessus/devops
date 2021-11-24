cf4840b848bed293c72464c3a32dd78dac87c965
De dessus Clement <clement.dedessuslesmous@student.vinci.ipl.be>
2021-11-24 16:18:47 +0100

    add - BerlinClockKata.php

--- BerlinClockKata/clock.php
+++ BerlinClockKata/clock.php
@@ -1,15 +1,47 @@
 <?php
 
-function clock()
+use PHPUnit\Framework\TestCase;
+
+class berlin_clock
 {
-
-    //ampoule seconde (pair/impaire)
-
-    //1er ligne heure
-    //2eme ligne heure 
-
-    //1er ligne minutes 
-
-    //2eme ligne minues 
-
+    public function minutes(int $number): string
+    {
+        $minute = '';
+        for ($i = 1; $i <= $number; $i++) {
+            $minute .= 'R';
+        }
+        return $minute;
+    }
+    public function fiveMinutes(int $number): string
+    {
+        $result=$number/5;
+        $minute = '';
+        for ($i = 1; $i <= $result; $i++) {
+            $minute .= 'R';
+        }
+        return $minute;
+    }
+    public function hours(int $number): string
+    {
+        $hour='';
+        for ($i = 1; $i <= $number; $i++) {
+            $hour .= 'R';
+        }
+        return $hour;
+    }
+    public function fiveHours(int $number): string
+    {
+        $result=$number/5;
+        $hour = '';
+        for ($i = 1; $i <= $result; $i++) {
+            $hour .= 'R';
+        }
+        return $hour;
+    }
+    public function seconds(int $number): string
+    {
+        $sec = 'OFF';
+        if($number%2==0){$sec='ON';}
+        return $sec;
+    }
 }
