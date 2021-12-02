28fc8423dc467128858fee116042d17b02027722
Clement De dessus <clement.dedessuslesmous@student.vinci.be>
2021-12-02 13:59:31 +0100

    add - berlinClockKata

---
+++ BerlinClockKata/berlinClockKata/berlinClockKataTest.php
@@ -0,0 +1,45 @@
+<?php
+require "berlinClockKata.php";
+
+use PHPUnit\Framework\TestCase;
+
+class berlinClockKataTest extends TestCase
+{
+    public function testMinutes()
+    {
+        $minute = new berlinClockKata();
+        $actual = $minute->minutes(0);
+        $this->assertEquals("", $actual);
+    }
+
+    public function testFiveMinutes()
+    {
+        $minute = new berlinClockKata();
+        $actual = $minute->fiveMinutes(0);
+        $this->assertEquals("", $actual);
+    }
+    public function testHours()
+    {
+        $hour = new berlinClockKata();
+        $actual = $hour->hours(0);
+        $this->assertEquals("", $actual);
+    }
+    public function testFiveHours()
+    {
+        $hour = new berlinClockKata();
+        $actual = $hour->fiveHours(1);
+        $this->assertEquals("", $actual);
+    }
+    public function testSeconds()
+    {
+        $second = new berlinClockKata();
+        $actual = $second->seconds(3);
+        $this->assertEquals("0", $actual);
+    }
+    public  function testAllClock()
+    {
+        $second = new berlinClockKata();
+        $actual = $second->heureEntiere(00,00,01);
+        $this->assertEquals("    0", $actual);
+    }
+}
--- FizzBuzz/vendor/composer/installed.php
+++ FizzBuzz/vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'aad60d563038ae35aeb77347b5850e2a9dba8b0c',
+        'reference' => '455693e1b2de75af9baf46fab8f0b069bd0d7628',
         'name' => '__root__',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'aad60d563038ae35aeb77347b5850e2a9dba8b0c',
+            'reference' => '455693e1b2de75af9baf46fab8f0b069bd0d7628',
             'dev_requirement' => false,
         ),
         'doctrine/instantiator' => array(
